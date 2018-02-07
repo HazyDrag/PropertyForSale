1981cbdb0187749d1f64f0cf3e52ede533317f3d
Дмитрий <goldrag46@gmail.com>
2018-02-07 15:34:34 +0300

    Finish mapping

---
+++ PropertyForSale/Models/ForgotViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PropertyForSaleDomainModel
+{
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public String Email { get; set; }
+    }
+}
